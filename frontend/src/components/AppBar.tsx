1772811b304e9f03146e9728135584f9ee3a147d
Taqi <mohammadtaqi5626@gmail.com>
2024-07-15 19:14:27 +0530

    updated appbar

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
 
 export const AppBar = () => {
     return <div className="border-b flex justify-between px-10 py-4">
-        <div className="flex flex-col justify-center">
+        <Link to={'/blogs'} className="flex flex-col justify-center">
             Maadhyam
-        </div>
+        </Link>
         <div>
+            <Link to={'/publish'}>
+                <button className="mr-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded-3xl">
+                    New Blog
+                </button>
+            </Link>
             <Avatar size="big" name="Taqi" />
         </div>
     </div>
