4e72f2909c44e548ed92b650ed7f942d03278476
Taqi <mohammadtaqi5626@gmail.com>
2024-07-15 19:13:39 +0530

    minor updates

--- frontend/src/components/CompleteBlog.tsx
+++ frontend/src/components/CompleteBlog.tsx
@@ -1,20 +1,42 @@
 import { Blog } from "../hooks"
 import { AppBar } from "./AppBar"
+import { Avatar } from "./BlogCard"
 
 export const CompleteBlog = ({ blog }: { blog: Blog }) => {
     return <div>
         <AppBar />
-        <div className="grid grid-cols-6 px-10 w-full bg-yellow-400">
-            <div className="bg-red-200 col-span-4">
-                <div>
-                    {blog.title}
+        <div className="flex justify-center">
+
+            <div className="grid grid-cols-6 px-20 w-full max-w-screen-xl py-10">
+                <div className="col-span-4">
+                    <div className="text-5xl font-extrabold">
+                        {blog.title}
+                    </div>
+                    <div className="text-slate-400 py-2">
+                        Posted on December 2023
+                    </div>
+                    <div>
+                        {blog.content}
+                    </div>
                 </div>
-                <div>
-                    {blog.content}
+                <div className="col-span-2">
+                    <div className="text-slate-600 text-lg">
+                        Author
+                    </div>
+                    <div className="flex w-full">
+                        <div className="pr-4 flex flex-col justify-center">
+                            <Avatar size="big" name={blog.author.name || "Anonymous"} />
+                        </div>
+                        <div>
+                            <div className="text-xl font-bold">
+                                {blog.author.name || "Anonymous"}
+                            </div>
+                            <div className="pt-2 text-slate-500">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a nihil sit praesentium sequi dolorem rem possimus amet, vel eveniet.
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="bg-green-200 col-span-2">
-                hello
             </div>
         </div>
     </div>
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,19 +1,19 @@
 import { useParams } from "react-router-dom";
 import { useBlog } from "../hooks"
 import { CompleteBlog } from "../components/CompleteBlog";
+import { AppBar } from "../components/AppBar";
 
 export const Blog = () => {
-    const { id } = useParams<{ id: string }>();
+    const { id } = useParams();
     const { loading, blog } = useBlog({
         id: id || ""
     });
-    if (loading) {
+    if (loading || !blog) {
         return <div>
+            <AppBar />
             loading...
         </div>
     }
-
-
 
     return <div>
         <CompleteBlog blog={blog} />
