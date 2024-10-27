d07f640711598b154cbb82f6127c4c2286b00c44
Taqi <mohammadtaqi5626@gmail.com>
2024-10-27 21:04:34 +0530

    v1

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -3,13 +3,13 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg px-2">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns."
+                    "Blogging is the modern diary, where thoughts become pixels, and every post is a step toward understanding ourselves and the world around us."
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
+                    Taqi Khan
                 </div>
                 <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
+                    CTO | Madhyam Inc.
                 </div>
             </div>
         </div>
